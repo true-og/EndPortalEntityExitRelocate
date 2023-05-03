3bb99fa28218a5ea6acab043e151f9189114f0ab
Gradyn Wursten <me@gradyn.com>
2023-05-02 19:37:49 -0600

    yea

--- src/main/java/com/gradyn/endportalentityexitrelocate/EndPortalEntityExitRelocate.java
+++ src/main/java/com/gradyn/endportalentityexitrelocate/EndPortalEntityExitRelocate.java
@@ -1,13 +1,23 @@
 package com.gradyn.endportalentityexitrelocate;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EndPortalEntityExitRelocate extends JavaPlugin {
+import java.util.logging.Logger;
 
+public final class EndPortalEntityExitRelocate extends JavaPlugin {
+    public static FileConfiguration config;
+
+    @Override
+    public void onLoad() {
+        this.saveDefaultConfig();
+        config = this.getConfig();
+        Logger.getAnonymousLogger().info("EndPortalEntityExitReloacate Loaded");
+    }
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new PortalExitListener(), this);
     }
 
     @Override
--- src/main/java/com/gradyn/endportalentityexitrelocate/PortalExitListener.java
+++ src/main/java/com/gradyn/endportalentityexitrelocate/PortalExitListener.java
@@ -1,2 +1,24 @@
-package com.gradyn.endportalentityexitrelocate;public class PortalExitListener {
+package com.gradyn.endportalentityexitrelocate;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalExitEvent;
+
+public class PortalExitListener implements Listener {
+    @EventHandler
+    public final void onEntityPortalExitEvent(EntityPortalExitEvent event) {
+        if (event.getFrom().getWorld().getEnvironment() != World.Environment.THE_END) return;
+        if (event.getTo().getWorld().getEnvironment() != World.Environment.NORMAL) return;
+        event.setTo(
+                new Location(
+                        Bukkit.getWorld(EndPortalEntityExitRelocate.config.getString("world")),
+                        EndPortalEntityExitRelocate.config.getDouble("location.x"),
+                        EndPortalEntityExitRelocate.config.getDouble("location.y"),
+                        EndPortalEntityExitRelocate.config.getDouble("location.z")
+                        ));
+    }
 }
